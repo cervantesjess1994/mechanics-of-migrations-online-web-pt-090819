b65dc904b496356d6d5ad2e7d615c411153e218b
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-11-28 06:53:48 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,7 @@ require 'active_record'
 require 'yaml/store'
 require 'ostruct'
 require 'date'
-
+require_relative "../artist.rb"
 
 require 'bundler/setup'
 Bundler.require
@@ -24,5 +24,3 @@ sql = <<-SQL
 SQL
 
 ActiveRecord::Base.connection.execute(sql)
-
-require_relative "../artist.rb"
